24f57a0d0c2a16b4d70631a8308bed7f94d4ad1e
vladislavpolar <vlad.tsybuliak.vt@gmail.com>
2021-06-17 17:20:34 +0300

    added 3 components. will be using inside a chat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { ChatEngine } from 'react-chat-engine'
 import './App.css'
+import ChatFeed from './components/ChatFeed'
 
 
 function App() {
   return (
     <ChatEngine
       height="100vh"
-      projectID=""
-      userName=""
-      userSecret=""
+      projectID="86a40797-ff1d-4630-b99c-02c3fc2ca0ea"
+      userName="Vladislavpolar"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   )
 }
