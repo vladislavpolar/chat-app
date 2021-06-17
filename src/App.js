639d9d499258a505d0efec8213d7d88590074028
vladislavpolar <vlad.tsybuliak.vt@gmail.com>
2021-06-17 16:53:57 +0300

    starting working with chat engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import { ChatEngine } from 'react-chat-engine'
+import './App.css'
 
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID=""
+      userName=""
+      userSecret=""
+    />
   )
 }
 
