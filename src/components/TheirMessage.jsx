36864c5b3149095cd57b17543f0b1a2a599ec6df
vladislavpolar <vlad.tsybuliak.vt@gmail.com>
2021-06-18 17:24:28 +0300

    added received icons, sending attachments, chat input, sending messages

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,35 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+  const ifFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username
+
   return (
-    <div>
-      TheirMessage
+    <div className="message-row">
+      {ifFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {message?.attachments?.length > 0
+        ? (
+          <img
+            src={message.attachments[0].file}
+            alt="message-attachment"
+            className="message-image"
+            style={{ marginLeft: ifFirstMessageByUser ? '4px' : '48px' }}
+          />
+        ) : (
+          <div
+            className="message"
+            style={{
+              float: 'left',
+              backgroundColor: '#CABCDC',
+              marginLeft: ifFirstMessageByUser ? '4px' : '48px'
+            }}
+          >
+            {message.text}
+          </div>
+        )
+      }
     </div>
   )
 }
